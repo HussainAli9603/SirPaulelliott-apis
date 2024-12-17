aca4f9f9b3cde04e02f8fef55f4f6cc20293ed2a
unknown <husnali1010@gmail.com>
2024-12-17 20:46:16 +0500

    first commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Load the Swagger YAML file
-const swaggerDocument = YAML.load('./swagger/swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger', '/swagger.yaml'));
 
 dotenv.config();
 const app = express();
